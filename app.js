fe1567301f5bdc10079fef1b444619e5bc918622
Cev Dev <alex-sdk@users.noreply.github.com>
2022-03-22 23:40:57 -0400

    form modal functionality

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-//TODO Button functionality Not Read/Remove
-//TODO Forum inputs/ submit button
-
+//todo: Implement local storage properly
+//todo: fix remove button
+//todo: display cards properly by not making same cards
 
 let myLibrary = [];
+
 const openModalButtons = document.querySelectorAll('[data-modal-target]');
 const closeModalButtons = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
+const submitButton = document.querySelector('.submit-btn');
 
 openModalButtons.forEach(button => {
     button.addEventListener('click', () => {
         const modal = document.querySelector("#modal");
+        document.getElementById('Author').value = "";
+        document.getElementById('Title').value = "";
+        document.getElementById('Genre').value = "";
         openModal(modal)
     });
 });
@@ -28,6 +33,17 @@ overlay.addEventListener('click', () => {
     });
 });
 
+submitButton.addEventListener('click', () => {
+    getInput()
+    displayBooks(myLibrary)
+});
+
+function getInput() {
+    const Author = document.getElementById("Author").value;
+    const Title = document.getElementById("Title").value;
+    const Genre = document.getElementById("Genre").value;
+    addBookToLibrary(Author, Title, Genre)
+}
 function openModal(modal) {
     if (modal == null) return;
     modal.classList.add('active');
@@ -49,9 +65,9 @@ function addBookToLibrary(Author, Title, Genre) {
 }
 function displayBooks(Arr) {
     const bookGrid = document.querySelector('.bookGrid');
-    for (let i = 0; i < Arr.length; i++){
+    for (let i = 0; i < Arr.length; i++) {
         const card = document.createElement('div');
-        card.className = 'card';
+        card.classList = 'card';
         bookGrid.appendChild(card)
         
         const contentAuthor = document.createElement('div');
@@ -67,29 +83,43 @@ function displayBooks(Arr) {
         card.appendChild(contentGenre);
 
         const buttonRead = document.createElement('button');
-        buttonRead.innerText = 'Not read';
+        buttonRead.innerText = 'Not Read';
+        buttonRead.classList = 'notRead';
         card.appendChild(buttonRead);
-
+        
         const removeButton = document.createElement('button');
         removeButton.innerText = 'Remove';
         card.appendChild(removeButton);
+
+        buttonRead.addEventListener('click', () => {
+            if (buttonRead.innerText == 'Read') {
+                buttonRead.innerText = 'Not Read';
+                buttonRead.classList.remove('read')
+                buttonRead.classList = 'notRead';
+            } else {
+                buttonRead.classList = 'read';
+                buttonRead.classList.remove('notRead');
+                buttonRead.innerText = 'Read';
+            }
+        });
+
+        removeButton.addEventListener('click', () => {
+
+        });
     }
 }
-addBookToLibrary(
-    "J.K Rowling",
-    "Harry Potter and The Chamber of Secrets",
-    "Ficton"
-);
-
-addBookToLibrary(
-    "George Orwell",
-    "1984",
-    "Science Fiction"
-)
-
-addBookToLibrary(
-    "Ayn Rand",
-    "Fountain Head",
-    "Fiction"
-)
-displayBooks(myLibrary);
+function setData() {
+    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));    
+}
+    
+function restore() {
+    if (!localStorage.myLibrary) {
+        displayBooks(myLibrary)
+    }else {
+        let objects =localStorage.getItem('myLibrary');
+        objects = JSON.parse(objects);
+        myLibrary = objects;
+        displayBooks(myLibrary)
+        }
+}
+restore()
